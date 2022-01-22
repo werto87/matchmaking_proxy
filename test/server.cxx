60e2a3bdbe3c888a6973e848d8d7e3d7869f63a6
werto87 <huhulasmichinruhe@web.de>
2022-01-23 00:16:52 +0100

    check messages from game

--- test/server.cxx
+++ test/server.cxx
@@ -41,7 +41,7 @@ using namespace boost::asio;
 
 typedef boost::beast::websocket::stream<boost::asio::use_awaitable_t<>::as_default_on_t<boost::beast::tcp_stream>> Websocket;
 boost::asio::awaitable<void>
-connectWebsocket (io_context &ioContext, boost::asio::ip::tcp::endpoint const &endpoint, std::filesystem::path const &pathToSecrets)
+connectWebsocket (io_context &ioContext, boost::asio::ip::tcp::endpoint const &endpoint, std::filesystem::path const &pathToSecrets, std::vector<std::string> &messagesFromGame)
 {
   try
     {
@@ -62,9 +62,9 @@ connectWebsocket (io_context &ioContext, boost::asio::ip::tcp::endpoint const &e
           co_await connection->async_write (boost::asio::buffer (std::string{ "LoginAsGuest|{}" }), use_awaitable);
           flat_buffer buffer;
           auto myWebsocket = std::make_shared<MyWebsocket<SSLWebsocket>> (MyWebsocket<SSLWebsocket>{ std::move (connection) });
-
           using namespace boost::asio::experimental::awaitable_operators;
-          co_await(myWebsocket->readLoop ([myWebsocket, &ioContext] (const std::string &msg) {
+          co_await(myWebsocket->readLoop ([myWebsocket, &ioContext, &messagesFromGame] (const std::string &msg) {
+            messagesFromGame.push_back (msg);
             if (boost::starts_with (msg, "LoginAsGuestSuccess"))
               {
                 myWebsocket->sendMessage (objectToStringWithObjectName (user_matchmaking::JoinMatchMakingQueue{}));
@@ -79,7 +79,6 @@ connectWebsocket (io_context &ioContext, boost::asio::ip::tcp::endpoint const &e
               }
             else if (boost::starts_with (msg, "LeaveGameSuccess"))
               {
-                // TODO create a test where this has to be called CHECK_IS_CALLED OR SOMETHING
                 ioContext.stop ();
               }
           }) && myWebsocket->writeLoop ());
@@ -121,8 +120,21 @@ TEST_CASE ("server", "[integration]")
     auto gameEndpoint = boost::asio::ip::tcp::endpoint{ ip::tcp::v4 (), gamePort };
     using namespace boost::asio::experimental::awaitable_operators;
     co_spawn (io_context, server.userMatchmaking (userEndpoint, pathToSecrets) || server.gameMatchmaking (gameEndpoint), printException);
-    co_spawn (io_context, connectWebsocket (io_context, userEndpoint, pathToSecrets), printException);
-    co_spawn (io_context, connectWebsocket (io_context, userEndpoint, pathToSecrets), printException);
+    auto messagesFromGamePlayer1 = std::vector<std::string>{};
+    co_spawn (io_context, connectWebsocket (io_context, userEndpoint, pathToSecrets, messagesFromGamePlayer1), printException);
+    auto messagesFromGamePlayer2 = std::vector<std::string>{};
+    co_spawn (io_context, connectWebsocket (io_context, userEndpoint, pathToSecrets, messagesFromGamePlayer2), printException);
     io_context.run ();
+    CHECK (messagesFromGamePlayer1.size () == 4);
+    CHECK (boost::starts_with (messagesFromGamePlayer1.at (0), "LoginAsGuestSuccess"));
+    CHECK (messagesFromGamePlayer1.at (1) == "JoinMatchMakingQueueSuccess|{}");
+    CHECK (messagesFromGamePlayer1.at (2) == "AskIfUserWantsToJoinGame|{}");
+    CHECK (messagesFromGamePlayer1.at (3) == "ProxyStarted|{}");
+    CHECK (messagesFromGamePlayer2.size () == 5);
+    CHECK (boost::starts_with (messagesFromGamePlayer2.at (0), "LoginAsGuestSuccess"));
+    CHECK (messagesFromGamePlayer2.at (1) == "JoinMatchMakingQueueSuccess|{}");
+    CHECK (messagesFromGamePlayer2.at (2) == "AskIfUserWantsToJoinGame|{}");
+    CHECK (messagesFromGamePlayer2.at (3) == "ProxyStarted|{}");
+    CHECK (messagesFromGamePlayer2.at (4) == "LeaveGameSuccess|{}");
   }
 }

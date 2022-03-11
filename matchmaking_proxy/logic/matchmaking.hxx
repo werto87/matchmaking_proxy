281a5d1ca8bcd4244433f6dfc0eff7e4afface69
werto87 <huhulasmichinruhe@web.de>
2022-03-11 20:41:51 +0100

    removed not declaration wich has no definition

--- matchmaking_proxy/logic/matchmaking.hxx
+++ matchmaking_proxy/logic/matchmaking.hxx
@@ -38,8 +38,6 @@ public:
 
   std::vector<std::string> currentStatesAsString () const;
 
-  std::string stateMachineAsString () const;
-
   std::unique_ptr<StateMachineWrapper, StateMachineWrapperDeleter> sm; // only use this member inside of matchmaking.cxx. reason because of incomplete type
 };
 

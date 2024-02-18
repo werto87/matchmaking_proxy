e2c17a7b9fa86b729841e03d0874b031df871e63
werto87 <huhulasmichinruhe@web.de>
2024-02-18 21:04:06 +0900

    wip remove range-v3 and replace it with std:range

--- conanfile.py
+++ conanfile.py
@@ -12,14 +12,12 @@ class Project(ConanFile):
         self.options["boost"].header_only = True
         # soci:with_sqlite3=True
 
-
     def requirements(self):
-        self.requires("boost/1.84.0",force=True)
+        self.requires("boost/1.84.0", force=True)
         self.requires("catch2/2.13.9")
-        self.requires("durak/1.0.2",force=True)
+        self.requires("durak/1.0.2", force=True)
         self.requires("confu_soci/0.3.20")
         self.requires("magic_enum/[>=0.9.5 <10]")
-        self.requires("range-v3/0.12.0")
         self.requires("certify/cci.20201114")
         self.requires("libsodium/1.0.18")
         self.requires("confu_json/1.0.1")

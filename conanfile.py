800331cae1ec56553186d7d9e0324ef4174dd25f
werto87 <huhulasmichinruhe@web.de>
2024-06-17 22:57:07 +0200

    reproducible builds

--- conanfile.py
+++ conanfile.py
@@ -29,7 +29,7 @@ class Project(ConanFile):
         self.requires("confu_json/1.1.0", force=True)
         self.requires("sml/1.1.11")
         self.requires("range-v3/0.12.0")
-        self.requires("login_matchmaking_game_shared/latest")
+        self.requires("login_matchmaking_game_shared/0.0.0")
         ### only for testing please do not put this in the package build recept ###
         self.requires("modern_durak_game_option/latest")
         self.requires("catch2/2.13.9")
